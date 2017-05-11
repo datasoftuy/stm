d0f2deacfa09d8cacda479a22010708860627134
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-11 11:02:33 -0300

    solo menu

---
+++ stm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese 1 para Jubilados A");
+            Console.WriteLine("Ingrese 2 para Jubilados B");
+            Console.WriteLine("Ingrese 3 para Estudiantes A");
+            Console.WriteLine("Ingrese 4 para Estudiantes B");
+        
+        
+        
+        
+        
+        }
+    }
+}
