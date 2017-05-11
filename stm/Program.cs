85db23a62905892882b1a13239b1ed8c0ac85bd0
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-11 12:21:57 -0300

    Boleto comun terminado

--- stm/Program.cs
+++ stm/Program.cs
@@ -10,14 +10,87 @@ namespace stm
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Ingrese 1 para Jubilados A");
-            Console.WriteLine("Ingrese 2 para Jubilados B");
-            Console.WriteLine("Ingrese 3 para Estudiantes A");
-            Console.WriteLine("Ingrese 4 para Estudiantes B");
+
+
+            const int precioboleto = 33;
+            int cargacomun;
+            int cargajubA;
+            int cargajuB;
+            int cargaestA;
+            int cargaestB;
+            string opcion;
+
+    
+
+            
+            
+            Console.WriteLine("*** Sistema de carga de STM ***\n\n");
+
+            Console.WriteLine("Menu Principal Ingrese la opcion\n");
+            
+            Console.WriteLine("Ingrese 1 para Jubilados A\n");
+            Console.WriteLine("Ingrese 2 para Jubilados B\n");
+            Console.WriteLine("Ingrese 3 para Estudiantes A\n");
+            Console.WriteLine("Ingrese 4 para Estudiantes B\n");
+            Console.WriteLine("Cargar STM boletos cmunes\n");
+            opcion = (Console.ReadLine());
+
+
+            switch (opcion)
+
+            {
+            
+                case "1":
+
+                    Console.WriteLine("Ingrese monto");
+                    cargacomun = Convert.ToInt32(Console.ReadLine());
+
+                    if (cargacomun == 200 && cargacomun == 1000)
+                    {
+
+                        Console.WriteLine("Se acredito: " + cargacomun);
+                        Console.ReadLine();
+                    }
+
+                    else
+                    {
+
+                        Console.WriteLine("Las cargas habilitadas son 200 y 1000");
+                        Console.ReadLine();
+                    }
+                
+                    
+                    break;
+
+                case "2":
+
+
+
+
+
+                    break;
+
+
+
+
+                default: Console.WriteLine("No es una opcion"+ opcion);
+                    Console.ReadLine();
+                    break;
+            
+            
+            
+            
+            
+            
+            
+            }
         
-        
-        
-        
+            
+            
+            
+            
+            
+            Console.ReadKey();
         
         }
     }
